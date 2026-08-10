5c3bb38866b910db8e3424b33457ee32011333ae
thushani <9thushani7@gmail.com>
2026-08-10 15:31:44 +0800

    Reverse String II - Optimized code

--- src/main/java/org/example/twoPointer/ReverseStringII.java
+++ src/main/java/org/example/twoPointer/ReverseStringII.java
@@ -1,11 +1,46 @@
 package org.example.twoPointer;
 
+
+
 public class ReverseStringII {
     public static void main(String[] args){
-        System.out.println(reverseStr("abcdefgh",4));
+        System.out.println(reverseStr("abcde",4));
 
     }
-    public static String reverseStr(String s, int k) {
+
+    public static String reverseStr(String s, int k){
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < s.length(); i+= 2*k) {
+            int start = i;
+            int end= Math.min(i+k-1, s.length()-1);
+            while ( start<end){
+                char temp = arr[start];
+                arr[start++] = arr[end];
+                arr[end--] = temp;
+            }
+        }
+        return new String(arr);
+    }
+    public static String reverseStr2(String s, int k){
+        char [] arr = s.toCharArray();
+        for (int i = 0; i < s.length(); i+= 2 * k) {
+            if(i+k < arr.length ){
+                reverse(i, i+k-1, arr);
+            }else{
+                reverse(i, s.length()-1,arr);
+            }
+        }
+        return new String(arr);
+    }
+
+    public static void reverse(int start, int end, char[] arr){
+        while (start<end){
+            char temp = arr[start];
+            arr[start++] = arr[end];
+            arr[end--] = temp;
+        }
+    }
+    public static String reverseStr1(String s, int k) {
         int shift = 2 * k;
         int iterations = s.length()/shift;
         int remainder = s.length()%shift;
