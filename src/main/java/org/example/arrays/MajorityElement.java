e6e7fc6bec9916b97043f9a5feda9993d2ca8812
thushani <9thushani7@gmail.com>
2026-08-23 23:28:04 +0800

    Majority element , RansomeNote

--- src/main/java/org/example/arrays/MajorityElement.java
+++ src/main/java/org/example/arrays/MajorityElement.java
@@ -7,14 +7,16 @@ import java.util.Map;
 public class MajorityElement {
 
     public static void main(String[] args){
-        System.out.println(majorityElement(new int[]{2,2,1,1,1,2,2}));
+        System.out.println(majorityElement(new int[]{2,2,1,1,1,2,2,1,1}));
     }
 
     public static int majorityElement(int[] nums) {
         Map<Integer,Integer> map = new HashMap<>();
         for (int num : nums) {
             map.put(num, map.getOrDefault(num,0)+1);
-            if(map.get())
+            if(map.get(num)>nums.length/2){
+                return num;
+            }
         }
         return 0;
     }
--- src/main/java/org/example/arrays/RansomNote.java
+++ src/main/java/org/example/arrays/RansomNote.java
@@ -1,4 +1,63 @@
 package org.example.arrays;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.HashMap;
+
 public class RansomNote {
+
+    public static void main(String[] args){
+        System.out.println(canConstruct("aab","aba"));
+    }
+
+
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character,Integer> mapRandsomNote  = new HashMap<>();
+        for(int i = 0 ; i<magazine.length() ; i++){
+            mapRandsomNote.put(magazine.charAt(i),mapRandsomNote.getOrDefault(magazine.charAt(i),0)+1);
+        }
+
+        for (int i=0; i< ransomNote.length(); i++ ){
+            char c= ransomNote.charAt(i);
+            if (!mapRandsomNote.containsKey(c) || mapRandsomNote.get(c)<=0){
+                return false;
+            }
+            else if (mapRandsomNote.containsKey(c)){
+                mapRandsomNote.put(c,mapRandsomNote.get(c)-1);
+            }
+        }
+        return true;
+    }
+    public static boolean canConstruct1(String ransomNote, String magazine) {
+        String [] ransomNoteArr = ransomNote.split("");
+        String [] magazineArr = magazine.split("");
+
+        for(String s: ransomNoteArr){
+            for (int i = 0; i < magazineArr.length; i++) {
+                if(s.equals(magazineArr[i])) {
+                    magazineArr[i] = null;
+                    break;
+                } else if (!s.equals(magazineArr[i]) && i==magazineArr.length-1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
+
+
+/*
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true*/
