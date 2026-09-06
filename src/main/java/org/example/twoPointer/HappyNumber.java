77492076cec807d7a2e98d8582cae97a9b07d8dd
thushani <9thushani7@gmail.com>
2026-09-06 14:42:02 +0800

    Happy Number- 1st attempt

--- src/main/java/org/example/twoPointer/HappyNumber.java
+++ src/main/java/org/example/twoPointer/HappyNumber.java
@@ -1,10 +1,47 @@
 package org.example.twoPointer;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class HappyNumber {
-    public boolean isHappy(int n) {
-        boolean isHappyNo= false;
 
+    public static void main(String[] args){
+        System.out.println(isHappy(1111111));
+    }
+    public static boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        int val =n;
+        while (val!=1 ){
+                List<Integer> list=String.valueOf(val)
+                        .chars() // convert to int value of characters
+                        .map(c->c-'0')// get the int values
+                        .boxed()//
+                        .map(x->x*x)
+                        .toList();
 
-        return isHappyNo;
+                val= list.stream().mapToInt(x->x).sum();
+                if(!set.add(val)){
+                    break;
+                }
+                set.add(val);
+        }
+        return val == 1;
     }
 }
+
+
+/*
+Example 1:
+Input: n = 19
+Output: true
+Explanation:
+        1^2 + 9^2 = 82
+        8^2 + 2^2 = 68
+        6^2 + 8^2 = 100
+        1^2 + 0^2 + 0^2 = 1
+
+Example 2:
+Input: n = 2
+Output: false
+*/
